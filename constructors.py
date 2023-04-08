7533ebf99a5d92395913ecf4b35fe1dd0b36e005
ayebaishmo <ishmo255super@gmail.com>
2023-04-08 12:13:12 +0300

    Add two numbers using a constructor

--- constructors.py
+++ constructors.py
@@ -8,7 +8,7 @@ obj = GeekforGeeks()
 
 obj.print_Geek()"""
 
-class Person:
+"""class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -16,4 +16,18 @@ class Person:
 Person1 = Person("ishmo", "27")
 Person2 = Person("Dan", "29")
 print(Person1.name," " + Person2.name)
-print(Person1.age," " + Person2.age)
+print(Person1.age," " + Person2.age)"""
+
+class caregivers:
+    def __init__(self, number1, number2):
+        self.number1 = number1
+        self.number2 = number2
+        self.addsTwoNumbers()
+
+    def addsTwoNumbers(self):
+        self.total = self.number1 + self.number2
+        print(self.total)
+
+caregivers1 = caregivers(25, 35)
+caregivers2 = caregivers(39, 30)
+
