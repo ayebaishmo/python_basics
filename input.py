cfa65c582d67957378ddfda4ac57f59ab9008249
ayebaishmo <ishmo255super@gmail.com>
2023-04-12 06:22:27 +0300

    correct a typo

--- input.py
+++ input.py
@@ -1,7 +1,7 @@
 x = int(input("Please enter an integer: "))
 if x < 0:
     x = 0
-    print("Nageative changed to zero")
+    print("Nagative changed to zero")
 elif x == 0:
     print('Zero')
 elif x == 1:
