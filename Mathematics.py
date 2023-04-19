64beb609b0ce0c62f3d7ae958c9d4fc5226c87eb
ayebaishmo <ishmo255super@gmail.com>
2023-04-19 13:47:03 +0300

    Add some math formular

--- Mathematics.py
+++ Mathematics.py
@@ -1,7 +1,17 @@
 import math
 """got = math.cos(math.pi /4)
 print(got)
-"""
 
 nada = math.log(1024,2)
-print(nada)
+print(nada)"""
+
+"""x = min(26, 90, 45)
+y = max(26, 90, 45)
+print(x)
+print(y)"""
+
+# Calculating the perimenter of circle
+daimenter =50
+
+perimenter  = 2 * math.pi * daimenter
+print(perimenter)
