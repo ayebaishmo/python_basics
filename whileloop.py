031cbd411131812e6b57b3f91b23fae5c566b179
ayebaishmo <ishmo255super@gmail.com>
2023-04-20 09:25:40 +0300

    Learn  about while loop

---
+++ whileloop.py
@@ -0,0 +1,10 @@
+# Calculating the squareroot of numbers 0 - 100
+
+from math import *
+
+i = 0 # innitial statement (starting point)
+
+while i <= 100: # condition
+    print(i, sqrt(i)) #block of code to calculate the sqrt
+    i = i + 1 # keep increasing the number until it satisfys the condition
+print("Ready")  
